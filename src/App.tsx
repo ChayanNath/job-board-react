f437844883ab21f159c7a54f57fda6cdae9642be
Chayan <chayannath19@gmail.com>
2025-12-05 23:45:45 +0530

    Updated add and edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Layout } from "./components/layout/Layout";
 import { ThemeProvider } from "./components/theme-provider";
 import { JobDashboard } from "./features/jobs/JobDashboard";
+import { Toaster } from "@/components/ui/sonner";
+
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Layout>
         <JobDashboard />
       </Layout>
+      <Toaster />
     </ThemeProvider>
   );
 }
--- src/features/jobs/JobDashboard.tsx
+++ src/features/jobs/JobDashboard.tsx
@@ -1,18 +1,22 @@
-import { mockJobs } from "@/mockData/mock-jobs";
 import { JobFilters } from "./JobFilters";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { Job, JobStatus } from "@/types/job";
 import { useDebounce } from "@/hooks/useDebounce";
 import { JobList } from "./JobList";
 import { Button } from "@/components/ui/button";
 import { JobForm, type JobFormValues } from "../forms/JobForm";
+import axios from "axios";
+import { toast } from "sonner";
+
+const API_PATH = "http://localhost:3000/api/v1/jobs";
 
 export const JobDashboard = () => {
   const [jobStatus, setJobStatus] = useState<JobStatus>("all");
   const [searchTerm, setSearchTerm] = useState("");
   const [showForm, setShowForm] = useState(false);
-  const [jobs, setJobs] = useState<Job[]>(mockJobs);
-  const [editingJob, setEditingJob] = useState<Job>();
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [editingJob, setEditingJob] = useState<Job | null>();
+  const [loading, setLoading] = useState(false);
 
   const debouncedSearchTerm = useDebounce(searchTerm, 300);
 
@@ -20,6 +24,17 @@ export const JobDashboard = () => {
     setEditingJob(job);
     setShowForm(true);
   };
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      setLoading(true);
+      const response = await axios.get(API_PATH);
+      const responseData = response?.data?.jobs;
+      setJobs(responseData);
+      setLoading(false);
+    };
+    fetchJobs();
+  }, []);
 
   const filteredJobList = useMemo(() => {
     return jobs
@@ -33,9 +48,8 @@ export const JobDashboard = () => {
       });
   }, [jobs, jobStatus, debouncedSearchTerm]);
 
-  const handleAddJob = (values: JobFormValues) => {
-    const newJob: Job = {
-      id: crypto.randomUUID(),
+  const handleAddJob = async (values: JobFormValues) => {
+    const newJob = {
       company: values.company,
       role: values.role,
       status: values.status,
@@ -43,28 +57,47 @@ export const JobDashboard = () => {
       appliedOn: values.appliedOn.toISOString(),
     };
 
-    setJobs((prev) => [newJob, ...prev]);
+    try {
+      const response = await axios.post(API_PATH, newJob);
+      const editResponse = response.data.job;
+      setJobs((prev) => [editResponse, ...prev]);
+      toast(editResponse.message);
+    } catch (error) {
+      console.error(error);
+      toast("Failed to add job! Please try again later.");
+    }
+
     setShowForm(false);
   };
 
-  const handleUpdateJob = (values: JobFormValues) => {
+  const handleUpdateJob = async (values: JobFormValues) => {
     if (!editingJob) return;
-
-    setJobs((prev) =>
-      prev.map((job) =>
-        job.id === editingJob.id
-          ? {
-              ...job,
-              company: values.company,
-              role: values.role,
-              status: values.status,
-              description: values.description,
-              appliedOn: values.appliedOn.toISOString(),
-            }
-          : job
-      )
-    );
+    try {
+      const payload = {
+        company: values.company,
+        role: values.role,
+        status: values.status,
+        description: values.description,
+        appliedOn: values.appliedOn.toISOString(),
+      };
+      const response = await axios.patch(
+        `${API_PATH}/${editingJob.id}`,
+        payload
+      );
+      const { message, job } = response.data;
+      setJobs((prev) => prev.map((j) => (j.id === editingJob.id ? job : j)));
+      toast(message);
+      setEditingJob(null);
+      setShowForm(false);
+    } catch (error) {
+      console.error(error);
+      toast("Failed to add job! Please try again later.");
+    }
   };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <section className="p-4">
@@ -79,7 +112,7 @@ export const JobDashboard = () => {
           onSearchChange={setSearchTerm}
         />
         <span className="text-sm text-muted-foreground">
-          {mockJobs.length} total jobs
+          {jobs?.length} total jobs
         </span>
         <Button onClick={() => setShowForm((prev) => !prev)}>
           {showForm ? "Close" : "Add Job"}
@@ -100,7 +133,7 @@ export const JobDashboard = () => {
                 }
               : undefined
           }
-          onSubmit={editingJob ? handleAddJob : handleUpdateJob}
+          onSubmit={editingJob ? handleUpdateJob : handleAddJob}
         />
       )}
       <JobList
